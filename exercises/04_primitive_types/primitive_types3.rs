f42e43cf036198867cc783c0ad364a8f807f6cbe
miridih-uhcho <uhcho@miridih.com>
2024-04-06 09:45:58 +0900

    solve 04_primitive_types

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,13 +5,17 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    // let mut a = Vec::new();
+    // for i in 0..100 {
+    //     a.push(i);
+    // }
+    let a = ["wow";100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
+        // println!("{}", a[0]); // ["wow", ... , "wow"]
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
         panic!("Array not big enough, more elements needed")
