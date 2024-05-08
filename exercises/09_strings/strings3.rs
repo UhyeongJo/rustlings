8c8fe9fd1d56a8580b5e5ce7b055f567ec26a436
miridih-uhcho <uhcho@miridih.com>
2024-05-08 22:08:24 +0900

    solve 09/10/11/12

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,27 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut result = String::from(input);
+
+    result.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this!
-    ???
+    let mut result = String::from(input);
+
+    result.push_str(" world!");
+    result.to_string()
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut result = String::from(input);
+
+    result.replace("cars", "balloons").to_string()
 }
 
 #[cfg(test)]
--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -14,8 +14,15 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
 }
+
+// hint
+// The compiler says a partial move happened in the `match` statement. How can
+// this be avoided? The compiler shows the correction needed.
+//
+// After making the correction as suggested by the compiler, do read:
+// https://doc.rust-lang.org/std/keyword.ref.html
